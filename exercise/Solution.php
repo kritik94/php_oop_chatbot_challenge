5eaf44ce8745d363205a1f86a055d98045eab665
Nikita Arhipov <niktyper13@gmail.com>
2016-04-02 16:29:53 +0700

    add servese response

--- exercise/Solution.php
+++ exercise/Solution.php
@@ -1,18 +1,38 @@
 <?php
 
 namespace App\Solution;
-
+/*
+ * if 'message' not found, return '::notFound' message
+ */
 class Bot
 {
     private $responseList = [];
+    private $serviseResponseList = [];
 
     public function addAnswer($message, $response)
     {
-        $this->responseList[$message] = $response;
+        if ($this->isServesMessage($message)) {
+            $this->serviseResponseList[$message] = $response;
+        } else {
+            $this->responseList[$message] = $response;
+        }
     }
 
     public function answer($message)
     {
+        if (!array_key_exists($message, $this->responseList)) {
+            return $this->serviseResponseList['::notFound']();
+        }
         return $this->responseList[$message]();
+    }
+
+    private function isServesMessage($message)
+    {
+        return (substr($message, 0, 2) == '::') ? true : false;
+        // if (preg_match('/^::/', $message) !== false) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     }
 }
