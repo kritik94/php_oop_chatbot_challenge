3f29415303177ae6588d3df8f03e35441236bb74
Nikita Arhipov <niktyper13@gmail.com>
2016-04-02 15:57:30 +0700

    add answer-question fuctional

--- exercise/Solution.php
+++ exercise/Solution.php
@@ -1,3 +1,18 @@
 <?php
 
 namespace App\Solution;
+
+class Bot
+{
+    private $responseList = [];
+
+    public function addAnswer($message, $response)
+    {
+        $this->responseList[$message] = $response;
+    }
+
+    public function answer($message)
+    {
+        return $this->responseList[$message]();
+    }
+}
--- exercise/TestSolution.php
+++ exercise/TestSolution.php
@@ -6,4 +6,14 @@ require_once 'Solution.php';
 
 class TestSolution extends \PHPUnit_Framework_TestCase
 {
+    public function testSimpleBot()
+    {
+        $bot = new Solution\Bot;
+        $bot->addAnswer('help', function() {
+            return "It's help message!";
+        });
+
+        $this->assertEquals("It's help message!", $bot->answer('help'));
+    }
 }
+// 'find {:name}' => params[name]
